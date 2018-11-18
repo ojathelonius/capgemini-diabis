db2606e75c249be8b2486f76ff074e42de55c3ab
ojathelonius <louis.johanet@epfedu.fr>
2018-11-18 18:44:06 +0100

    Add points counter to header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header, Left, Body, Right, Title } from 'native-base';
-import { Image, View, Container } from 'react-native';
+import { Image, View, Container, Text } from 'react-native';
 
 class MainHeader extends React.Component {
 
@@ -32,6 +32,18 @@ class MainHeader extends React.Component {
             flex: 1
         }
 
+        const pointStyle = {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            alignItems: 'center'
+        }
+
+        const textPointStyle = {
+            fontSize: 18,
+            color: '#7E5E41'
+        }
+
         return (
 
             <Header style={headerStyle}>
@@ -39,8 +51,14 @@ class MainHeader extends React.Component {
                     style={imgStyle}
                     resizeMode={'contain'}
                     source={require('../../assets/logo.png')} /></View>
-                <View style={{flex: 2}}><Title style={titleStyle}>{this.props.title ? this.props.title : ''}</Title></View>
-                <View style={flexStyle}></View>
+                <View style={{ flex: 2 }}><Title style={titleStyle}>{this.props.title ? this.props.title : ''}</Title></View>
+                <View style={pointStyle}>
+                    <Text style={textPointStyle}>89</Text>
+                    <Image
+                        style={imgStyle}
+                        resizeMode={'contain'}
+                        source={require('../../assets/icons/points.png')} />
+                </View>
             </Header>
         );
     }
