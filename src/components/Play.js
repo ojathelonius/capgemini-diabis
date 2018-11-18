516476375f3f1d2b84b28533292b919910da804a
ojathelonius <louis.johanet@epfedu.fr>
2018-11-18 18:06:26 +0100

    Add "Play" achievements

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View,Image, ScrollView } from 'react-native';
+import { List, Text, ListItem, Form, Item, Picker, Card, CardItem, Left, Thumbnail, Body, Right, Button, Icon } from 'native-base';
 
 class Play extends React.Component {
 
     render() {
 
         const containerStyle = {
-            flex: 1
+            flex: 1,
+            backgroundColor: 'white'
         };
+
+        const listStyle = {
+            marginBottom: 20
+        }
+
+        const titleStyle = {
+            fontSize: 13,
+            fontFamily: 'Luna',
+            color: '#7F5F42',
+            textAlign: 'center'
+        }
         return (
             <View style={containerStyle}>
-                <Text>Play</Text>
+                <ScrollView style={{ paddingTop: 10 }}>
+                    <Text style={titleStyle}>Achievements</Text>
+                    <List style={listStyle}>
+                        <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={require('../../assets/icons/3_days_row.png')} />
+                            </Left>
+                            <Body>
+                                <Text>A good start</Text>
+                                <Text note numberOfLines={2}>Measure your blood sugar 3 days in a row</Text>
+                            </Body>
+                            <Right>
+                                <Text>+ 3 points</Text>
+                            </Right>
+                        </ListItem>
+                        <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={require('../../assets/icons/5_days_row.png')} />
+                            </Left>
+                            <Body>
+                                <Text>Keep going...</Text>
+                                <Text note numberOfLines={2}>Measure your blood sugar 5 days in a row</Text>
+                            </Body>
+                            <Right>
+                                <Text>+ 5 points</Text>
+                            </Right>
+                        </ListItem>
+                        <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={require('../../assets/icons/7_days_row.png')} />
+                            </Left>
+                            <Body>
+                                <Text>What a champ !</Text>
+                                <Text note numberOfLines={2}>Measure your blood sugar 7 days in a row</Text>
+                            </Body>
+                            <Right>
+                                <Text>+ 7 points</Text>
+                            </Right>
+                        </ListItem>
+                    </List>
+                    <Text style={titleStyle}>Games</Text>
+                </ScrollView>
             </View >
         );
     }
